05cf9edc0fbb6c6a1204d73eca639ad78401a6cd
ADH <ADH@ADH-PC>
2023-08-04 08:11:31 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample
+{
+
+    public delegate string PersonToString(Person person);
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class PersonFullName
+    {
+        public static string GetPersonFullName(Person person) => $"{person.FirstName} {person.LastName}";
+    }
+    public class PersonFullNameReverse
+    {
+        public static string GetPersonFullName(Person person) => $"{person.LastName} {person.FirstName} ";
+    }
+    public class PersonPrinter
+    {
+        public void Print(Person person)
+        {
+            string s= PersonFullName.GetPersonFullName(person);
+            Console.WriteLine(s);
+        }
+
+        public void Print2(PersonToString personToString,Person person)
+        {
+            string s = personToString(person);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateSample;
+
+Console.WriteLine("Hello, World!");
+
+Person person = new Person
+{
+    FirstName = "Ata",
+    LastName = "Hoseini"
+};
+
+
+PersonPrinter personPrinter = new PersonPrinter();
+personPrinter.Print(person);
+Console.WriteLine("".PadLeft(100, '_'));
+
+personPrinter.Print2(PersonFullName.GetPersonFullName, person);
+personPrinter.Print2(PersonFullNameReverse.GetPersonFullName, person);
+Console.WriteLine("".PadLeft(100, '_'));
+
+Console.WriteLine("Delegate variable");
+PersonToString myDelegate = PersonFullName.GetPersonFullName;
+var result = myDelegate.Invoke(person);
+Console.WriteLine(result);
